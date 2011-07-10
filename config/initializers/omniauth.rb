88428f76b9b231fe2311538681b8e210732e8721
Matt Redding <controlfreak@laptopbattle.jp>
2011-07-10 15:03:33 +0900

    added omniauth soundcloud login link and keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'fVmohsS3LBUSZbG5ciweHA', 'eaQFzHQMQiJo91FpqAer4G5rkpGuggGRfQUPLfaNx5s'
+  provider :soundcloud, 'DeDNYH72GVOkmSU6gBtIDA', 'TZmM0XbY5NCfNBwM1Qq5zV0KAP3SZDyex9WqXOsDGE'
 end
