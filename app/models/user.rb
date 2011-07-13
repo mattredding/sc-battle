8b6736bbe29c5ddb376ee620646d71bd26a4b686
Matt Redding <controlfreak@laptopbattle.jp>
2011-07-13 15:54:45 +0900

    change user id to string

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   def self.create_with_omniauth(auth)
     create! do |user|
       user.provider = auth["provider"]
-      user.uid = auth["uid"]
+      user.uid = auth["uid"].to_s
       user.name = auth["user_info"]["name"]
     end
   end 
