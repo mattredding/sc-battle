cbd346fdf43837b0f363ebd9aed2be31134ddbca
Matt Redding <controlfreak@laptopbattle.jp>
2011-08-10 23:40:51 +0900

    battle show - set array of battle tracks in controller in view display track title and url link with sc-player class add sc-player and soundcloud.player.api widget javascripts add player images and css

--- app/controllers/battle_controller.rb
+++ app/controllers/battle_controller.rb
@@ -5,6 +5,12 @@ class BattleController < ApplicationController
 
   def show
     @battle = Battle.find_by_id(params[:id])
+    @battle_tracks = []
+    @battle.tracks.each do |track|
+      track_id = track.soundcloud_track_id
+      @battle_tracks.push( current_sc_user.soundcloud_client.get("/me/tracks/"+track_id.to_s) )
+    end
+
     if logged_in?
       @hide_join_button = !!@battle.tracks.find(:first, :conditions => [ "user_id = ?", @current_sc_user.id])
     else
