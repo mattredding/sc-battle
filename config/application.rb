85124f061feac085da4d7c0df20b1f65fc1fe711
Matt Redding <controlfreak@laptopbattle.jp>
2011-08-12 00:26:54 +0900

    change loading order of javascript in application.rb

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module ScBattle
     # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] = %w(jquery-1.6.2.min soundcloud.player.api sc-player )
+    config.action_view.javascript_expansions[:defaults] = %w(connect jquery-1.6.2.min soundcloud.player.api sc-player application)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
